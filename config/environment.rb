b81c2037bd50e5af923f5cec4f5ab1b55c12a38f
Andrew Johnson <ajohnson.uva@gmail.com>
2019-01-12 15:49:26 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,20 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
 
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
-# put the code to connect to the database here
-
+DB = ActiveRecord::Base.connection
+# DB.execute(<<~SQL)
+# CREATE TABLE IF NOT EXISTS artists (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   genre TEXT,
+#   age INTEGER,
+#   hometown TEXT
+# );
+# SQL
 
 require_relative "../artist.rb"
